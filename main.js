aeb247668838b836545ada7eac0c33ea731e720c
zya <ziyaeddiny@gmail.com>
2014-01-21 12:44:23 +0000

    Added Percussion

--- main.js
+++ main.js
@@ -31,39 +31,52 @@ window.onload = function(){
 	
 	
 	//generate search params - query,tags,durationfrom,durationto
-	var kickParams = generateParameters("kick","kick,",100,3000);
-	var snareParams = generateParameters("snare","snare,",100,2000);
+	var kickParams = generateParameters("kick","kick,",100,5000);
+	var snareParams = generateParameters("snare","snare,",100,5000);
 	var hatParams = generateParameters("hihat","hihat,",100,1000);
-	var sampleParams = generateParameters("pad","",5000,20000);
+	//fx - pad - choir
+	var sampleParams = generateParameters("pad","",Math.round((Math.random() * 50000) + 5000),60000);
+	var percParams = generateParameters("percussion","",100,3000);
 	//load the sounds
 	var kick = new Sound(context,kickParams);
 	var snare = new Sound(context,snareParams);
 	var hat = new Sound(context,hatParams);
+	var perc = new Sound(context,percParams);
 	var sample = new Sound(context,sampleParams);
 	
-	
-	var speed = (Math.random() * 4) + 1;
-	var eightNote = speed / 3;
+	//choose a random tempo
+	var speed = (Math.random() * 3) + 0.8;
+	var quarterNote = speed / 4;
 	
 	var l = new loop(function(next){
 		kick.start(next);
-		kick.stop(next + eightNote);
-		snare.start(next + (eightNote * 2));
-
+		kick.stop(next + quarterNote);
+		snare.start(next + (quarterNote * 2));
+		perc.start(next + (quarterNote * 3));
+		perc.stop(next + (quarterNote * 4));
 		hat.start(next);
-		hat.start(next + eightNote);
-		hat.start(next + eightNote * 2);
-		hat.start(next + eightNote * 3);
-		hat.start(next + eightNote * 4);
-		
-		
-		sample.start(next,Math.random() * (sample.buffer.duration / 1.5));
-		sample.stop(next + eightNote * 4);
-		sample.start(next + eightNote * 8,Math.random() * sample.buffer.duration);
-		sample.stop(next + 2);
+		hat.start(next + quarterNote);
+		hat.start(next + quarterNote * 2);
+		hat.start(next + quarterNote * 3);
+		//hat.start(next + eightNote * 4);
 
-		
+		if(sample.loaded){
+			var offset = Math.random() * (sample.buffer.duration / 1.5);
+			sample.start(next,offset);
+			sample.stop(next + speed);
 
+			sample.source.playbackRate.value = 0.5;
+			sample.start(next,offset);
+			sample.stop(next + speed);
+
+			sample.source.playbackRate.value = 2;
+			sample.start(next,offset);
+			sample.stop(next + speed);
+		
+		}else{
+			console.log('sample not loaded yet');
+		}
+		
 
 	},0,speed,context);
 	
