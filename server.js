fadecb6f8a2d0953409d4644d786bd5c47a92fb7
zya <ziyaeddiny@gmail.com>
2014-02-20 12:10:19 +0000

    changed the host

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ cors_proxy.createServer({
 	requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 
-}).listen(8080,host,function(){
+}).listen(port,host,function(){
 	console.log('cors-anywhere is running on ' + host + ':' + 8080);
 });
 
