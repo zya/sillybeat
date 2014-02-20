849a6bf01ac94b7ee521b0b5a30c428fb5f93bfb
zya <ziyaeddiny@gmail.com>
2014-02-20 12:04:00 +0000

    added 0.0.0.0 as  host?

--- public/sound.js
+++ public/sound.js
@@ -17,7 +17,7 @@ function Sound(context,searchparameters,output,callbackfunction){
 		//make the url and prepare it for proxying
 		var url = tracks[random].stream_url + '?client_id=e553081039dc6507a7c3ebf6211e4590';
 		that.permalink = tracks[random].permalink_url;
-		that.url = 'http://sillybeat.herokuapp.com:8080/' +  url;
+		that.url = 'http://0.0.0.0:8080/' +  url;
 		
 		//load the buffer - decode it and return buffer
 		that.request = new XMLHttpRequest();
@@ -34,7 +34,7 @@ function Sound(context,searchparameters,output,callbackfunction){
 				var random = Math.floor(Math.random() * tracks.length); //choose a random one
 				//make the url and prepare it for proxying
 				var url = tracks[random].stream_url + '?client_id=e553081039dc6507a7c3ebf6211e4590';
-				that.url = 'http://sillybeat.herokuapp.com:8080/' +  url;
+				that.url = 'http://0.0.0.0:8080/' +  url;
 				that.request.open('GET', that.url, true);
 				that.request.send();
 			});
