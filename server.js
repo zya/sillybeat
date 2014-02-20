b0a5bf3ecd0186ba8a3a76074bf2437f5132ca65
zya <ziyaeddiny@gmail.com>
2014-02-20 12:02:12 +0000

    added port

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var directory = '/public/';
 
 var app = connect().use(connect.static('public'));
 
-var port = Number(process.env.PORT || 8888);
+var port = Number(process.env.PORT || 5000);
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 console.log(port);
 
 http.createServer(app).listen(port);
@@ -17,5 +18,7 @@ cors_proxy.createServer({
 	requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 
-}).listen(8080);
+}).listen(8080,host,function(){
+	console.log('cors-anywhere is running on ' + host + ':' + 8080);
+});
 
